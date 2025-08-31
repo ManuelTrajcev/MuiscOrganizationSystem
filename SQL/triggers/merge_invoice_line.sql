01158a5be9da31bda5dbe9834332f51dc7988ae0
Manuel <manueltrajcev7@gmail.com>
2025-08-31 18:21:43 +0200

    2 triggers added

---
+++ SQL/triggers/merge_invoice_line.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION merge_invoice_line()
+RETURNS TRIGGER AS $$
+
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM invoice_line
+        WHERE invoice_id = NEW.invoice_id
+          AND track_id = NEW.track_id
+    ) THEN
+        UPDATE invoice_line
+        SET quantity = quantity + NEW.quantity
+        WHERE invoice_id = NEW.invoice_id
+          AND track_id = NEW.track_id;
+
+        RETURN NULL;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER trg_merge_invoice_line
+BEFORE INSERT ON invoice_line
+FOR EACH ROW
+EXECUTE FUNCTION merge_invoice_line();
+
+
+
+
---
+++ SQL/triggers/upsert_price.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE FUNCTION upsert_price()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM Price p
+        WHERE p.track_id = NEW.track_id
+          AND p.date = NEW.date
+    ) THEN
+        UPDATE Price
+        SET value = NEW.value
+        WHERE track_id = NEW.track_id
+          AND date = NEW.date;
+
+        RETURN NULL;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_upsert_price
+BEFORE INSERT ON Price
+FOR EACH ROW
+EXECUTE FUNCTION upsert_price();
+
+
